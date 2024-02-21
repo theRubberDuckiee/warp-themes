0523442f8f7e449378c7d002606a2ce5376a364a
Jess <jessica.wang360@gmail.com>
2024-02-20 21:55:52 -0800

    firebase working

--- pages/api/[tId].ts
+++ pages/api/[tId].ts
@@ -5,14 +5,14 @@ import YAML from 'json-to-pretty-yaml';
 import { db } from './create';
 // import { firebaseConfig } from './create';
 
-// const firebaseConfig = {
-// 	apiKey: process.env.FIREBASE_API_KEY,
-// 	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-// 	projectId: process.env.FIREBASE_PROJECT_ID,
-// 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-// 	appId: process.env.FIREBASE_APP_ID,
-// };
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+};
 
 // export const firebaseConfig = {
 // 	apiKey: "AIzaSyBzZfDNCqyrwUW8DvWSnEBn-Q-6mIzxADQ",
