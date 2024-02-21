f9d61082218b3b05f85630018e6a92f5342a916e
Jess <jessica.wang360@gmail.com>
2024-02-20 22:01:17 -0800

    fix firebase

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -2,24 +2,24 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-};
+// const firebaseConfig = {
+// 	apiKey: process.env.FIREBASE_API_KEY,
+// 	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+// 	projectId: process.env.FIREBASE_PROJECT_ID,
+// 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+// 	appId: process.env.FIREBASE_APP_ID,
+// };
 
-// export const firebaseConfig = {
-// 	apiKey: "AIzaSyBzZfDNCqyrwUW8DvWSnEBn-Q-6mIzxADQ",
-// 	authDomain: "warp-themes-cf724.firebaseapp.com",
-// 	projectId: "warp-themes-cf724",
-// 	storageBucket: "warp-themes-cf724.appspot.com",
-// 	messagingSenderId: "227546572873",
-// 	appId: "1:227546572873:web:4251b2470105d315223798",
-// 	measurementId: "G-YY8ELH4ZN6"
-//   };
+export const firebaseConfig = {
+	apiKey: "AIzaSyBzZfDNCqyrwUW8DvWSnEBn-Q-6mIzxADQ",
+	authDomain: "warp-themes-cf724.firebaseapp.com",
+	projectId: "warp-themes-cf724",
+	storageBucket: "warp-themes-cf724.appspot.com",
+	messagingSenderId: "227546572873",
+	appId: "1:227546572873:web:4251b2470105d315223798",
+	measurementId: "G-YY8ELH4ZN6"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
